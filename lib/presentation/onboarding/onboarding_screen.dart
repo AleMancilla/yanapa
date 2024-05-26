0f2338c0a0aa23c4154782572cbca8d3e3ebb0f3
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2024-05-26 17:21:22 -0400

    tutorial complete

--- lib/presentation/onboarding/onboarding_screen.dart
+++ lib/presentation/onboarding/onboarding_screen.dart
@@ -12,17 +12,17 @@ class _OnboardingScreenState extends State<OnboardingScreen>
   final PageController _pageController = PageController(initialPage: 0);
 
   final List<String> imageUrls = [
-    'assets/images/undraw_mobile_ux_re_59hr.png',
-    'assets/images/undraw_Emails_re_cqen.png',
-    'assets/images/undraw_Alert_re_j2op.png',
+    'assets/images/Yanapa/1_yanapa.gif',
+    'assets/images/Yanapa/2_yanapa.gif',
+    'assets/images/Yanapa/3_yanapa.gif',
   ];
 
-  final List<String> descriptions = [
-    '''YANAPA 
-    Reconocimiento de texto y análisis lingüístico: La app debe extraer texto de imágenes y analizarlo en busca de señales de fraude.''',
-    'Algoritmos avanzados de detección de fraudes: Debe usar técnicas de aprendizaje automático para identificar patrones sospechosos en el texto.',
-    'Actualización continua y adaptabilidad: Se mantiene al día con las últimas tácticas de estafadores y mejorar constantemente su capacidad de detección.',
-  ];
+  // final List<String> descriptions = [
+  //   '''YANAPA
+  //   Reconocimiento de texto y análisis lingüístico: La app debe extraer texto de imágenes y analizarlo en busca de señales de fraude.''',
+  //   'Algoritmos avanzados de detección de fraudes: Debe usar técnicas de aprendizaje automático para identificar patrones sospechosos en el texto.',
+  //   'Actualización continua y adaptabilidad: Se mantiene al día con las últimas tácticas de estafadores y mejorar constantemente su capacidad de detección.',
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +41,7 @@ class _OnboardingScreenState extends State<OnboardingScreen>
               itemBuilder: (context, index) {
                 return OnboardingPage(
                   imageUrl: imageUrls[index],
-                  description: descriptions[index],
+                  // description: descriptions[index],
                 );
               },
             ),
@@ -82,7 +82,7 @@ class _OnboardingScreenState extends State<OnboardingScreen>
               )
             ],
           ),
-          SizedBox(height: 50),
+          SizedBox(height: 20),
         ],
       ),
     );
@@ -94,9 +94,11 @@ class _OnboardingScreenState extends State<OnboardingScreen>
 
 class OnboardingPage extends StatelessWidget {
   final String imageUrl;
-  final String description;
+  // final String description;
 
-  OnboardingPage({required this.imageUrl, required this.description});
+  OnboardingPage({
+    required this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -105,21 +107,23 @@ class OnboardingPage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            imageUrl,
-            height: 300,
-            width: 300,
-            fit: BoxFit.contain,
-          ),
-          SizedBox(height: 20),
-          Text(
-            description,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+          Flexible(
+            child: Image.asset(
+              imageUrl,
+              // height: 300,
+              width: double.infinity,
+              fit: BoxFit.contain,
             ),
-            textAlign: TextAlign.left,
           ),
+          // SizedBox(height: 20),
+          // Text(
+          //   description,
+          //   style: TextStyle(
+          //     fontSize: 18,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          //   textAlign: TextAlign.left,
+          // ),
         ],
       ),
     );
